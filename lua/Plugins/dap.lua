e95683de91cedd097895ef70ab3a7ddaf4342250
Sqyd <wojkap10@proton.me>
2025-09-17 20:31:27 +0200

    Started debuger's config

--- lua/Plugins/dap.lua
+++ lua/Plugins/dap.lua
@@ -7,9 +7,21 @@ local Dap = {
 		"nvim-neotest/nvim-nio",
     },
 
-	config = function ()
+	config = function()
 		local dap = require("dap")
-		
+
+		-- Python config
+		dap.configuration.python = {
+			type = 'python';
+			request = 'launch';
+			name = 'Debug Python',
+			program = "${file}",
+			pythonPatch = function()
+				-- NOTE: Do so it takes the path from your system(So it works on nixos too)
+				return '/usr/bin/python'
+			end;
+		}
+
 		-- Here config(empty for now)
 
 
