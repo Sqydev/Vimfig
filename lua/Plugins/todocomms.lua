6a63672199cb1cde177ff95a2f73cae188117cb5
Sqyd <wojkap10@proton.me>
2025-09-17 22:45:34 +0200

    BTF!

--- lua/Plugins/todocomms.lua
+++ lua/Plugins/todocomms.lua
@@ -1,7 +1,7 @@
 local TodoComms = {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {  }
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {  }
 }
 
 return TodoComms
