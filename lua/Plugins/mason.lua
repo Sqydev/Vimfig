3835b3d0e7752bca490cf53b10ee88a98b29076b
Sqyd <wojkap10@proton.me>
2025-09-17 10:35:50 +0200

    I think lsp works

--- lua/Plugins/mason.lua
+++ lua/Plugins/mason.lua
@@ -1,28 +1,36 @@
 local Mason = {
-	"mason-org/mason.nvim",
-	opts = {}
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        "VonHeikemen/lsp-zero.nvim",
+    },
+    config = function()
+        local lsp_zero = require("lsp-zero")
+
+        lsp_zero.on_attach(function(_, bufnr)
+            lsp_zero.default_keymaps({buffer = bufnr})
+        end)
+
+        require("mason").setup({})
+        require("mason-lspconfig").setup({
+            automatic_installation = false, -- instalujesz ręcznie w TUI
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end,
+            },
+        })
+
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+        })
+    end,
 }
 
-function Mason.config()
-	local lsp = require("Plugins.lspzero")
-
-	lsp.preset("recommended")
-
-	lsp.set_preferences({
-        suggest_lsp_servers = true,
-        sign_icons = {
-            error = '✖',
-            warn = '▲',
-            hint = 'H',
-            info = 'I'
-        }
-    })
-
-	lsp.setup()
-
-	vim.diagnostic.config({
-        virtual_text = true
-    })
-end
-
 return Mason
+
