04c553f53db3981edd00b3884c417092ac5f475a
Sqyd <wojkap10@proton.me>
2025-09-21 00:52:18 +0200

    See in commit

--- lua/Plugins/nixos-lsp-supp.lua
+++
@@ -1,8 +0,0 @@
--- Experimental on nixos
-local lsp_zero = require('lsp-zero')
-
-vim.lsp.config("clangd", {
-    cmd = { "clangd", "--query-driver=/run/current-system/sw/bin/gcc" },
-    on_attach = lsp_zero.on_attach,
-    capabilities = lsp_zero.capabilities,
-})
